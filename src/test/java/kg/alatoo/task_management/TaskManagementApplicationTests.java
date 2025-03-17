0cb904f43cc3a9c8c1ec4b7ee4f500cd4fa11fa6
Aisuluu Sharipova <@asharipova>
2025-03-17 13:41:38 +0600

    Initial Spring Boot project setup with required dependencies

---
+++ src/test/java/kg/alatoo/task_management/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.task_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
