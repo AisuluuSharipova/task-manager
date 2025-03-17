d636937481222729e7b514a7ad7d0607d2a2e343
Aisuluu Sharipova <@asharipova>
2025-03-17 13:43:56 +0600

    User entity is added

---
+++ src/main/java/kg/alatoo/task_management/entities/User.java
@@ -0,0 +1,19 @@
+package kg.alatoo.task_management.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
